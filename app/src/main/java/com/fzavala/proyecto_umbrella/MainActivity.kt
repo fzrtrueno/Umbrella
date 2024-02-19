fc96cb4eb381fa5a637c5f74af7ddbddfecad4d0
Francisco Zavala <fzavala@utcam.edu.mx>
2024-02-19 09:20:27 -0600

    primera version del proyecto, realizada por el maestro

---
+++ app/src/main/java/com/fzavala/proyecto_umbrella/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fzavala.proyecto_umbrella
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
